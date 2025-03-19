b061676be5ea5695a7dfafedced3697aebb8851a
OmKuthe <omkuthe9thstd@gmail.com>
2025-03-19 23:59:31 +0530

    connected Database and added transaction model

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,11 @@
 import  express, { request }  from "express";
-const PORT=3000;
+import dotenv from 'dotenv';
+import mongoose from "mongoose";
+
+dotenv.config();
+const mongourl=process.env.mongourl;
+const PORT=process.env.PORT;
+
 
 const app = express();
 app.get('/',(request,response)=>{
@@ -7,6 +13,13 @@ app.get('/',(request,response)=>{
     return response.status(234).send("Hello OM");
 })
 
-app.listen(PORT,()=>{
-    console.log(`App is listning on ${PORT}`);
+mongoose.connect(mongourl).then(()=>{
+    console.log("Database Connected Succesfully")
+    app.listen(PORT,()=>{
+        console.log(`App is listning on ${PORT}`);
+    })
+}).catch((err)=>{
+    console.log("Failed to connect to Database");
+    console.log(err);
 })
+
---
+++ backend/models/transaction.js
@@ -0,0 +1,7 @@
+import mongoose, { Schema } from "mongoose";
+
+const transactionschema=new mongoose.Schema({
+
+});
+
+module.exports=mongoose.model('transaction',transactionschema);
