1cd761b30e9c72a5de7315c46d62ea3baedd75d1
OmKuthe <omkuthe9thstd@gmail.com>
2025-03-29 14:30:46 +0530

    Added Upload page

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,11 +2,13 @@ import React from "react"
 import {Routes,Route} from "react-router-dom"
 import Home from "./pages/Home"
 import Transactions from "./pages/Transactions"
+import Upload from "./pages/Upload"
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />}/>
+      <Route path="/upload" element={<Upload />}/>
       <Route path="/get" element={<Transactions />}/>
     </Routes>
   )
